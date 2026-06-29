f7e8bf6c26f3f623f5c5579862a6c1cebe553fd1
Evgeny Vylko <belfoot10@gmail.com>
2026-06-29 12:38:02 +0300

    change User entity

--- src/main/java/org/example/vylko/pp_3_1_1_springboot/entity/User.java
+++ src/main/java/org/example/vylko/pp_3_1_1_springboot/entity/User.java
@@ -1,4 +1,50 @@
 package org.example.vylko.pp_3_1_1_springboot.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
