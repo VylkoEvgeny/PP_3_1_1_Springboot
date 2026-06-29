12f37e451ed185a056d5b5015e22a59ea061536f
Evgeny Vylko <belfoot10@gmail.com>
2026-06-29 12:28:50 +0300

    initial commit

---
+++ src/main/java/org/example/vylko/pp_3_1_1_springboot/Pp311SpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.vylko.pp_3_1_1_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp311SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pp311SpringbootApplication.class, args);
+    }
+
+}
